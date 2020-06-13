5e1f3640adcba4b6e7bcbddb461d665e2bcae3da
Sreenivas Ananthakrishna <sreeni@gmail.com>
2020-06-13 14:32:31 +0530

    add script for consecutive k heads

---
+++ src/scripts/consecutive_k_heads.r
@@ -0,0 +1,26 @@
+flipCoin <- function(nFlips, pHead){
+  flippedCoins <- sample(0:1, nFlips, replace= TRUE,prob = c(pHead, 1-pHead))
+  return (flippedCoins)  
+}
+
+areThereConsecutiveHeads <- function(kHeads, flippedCoins){
+  pattern = c(array(0, kHeads),1)
+  expected = paste(pattern, collapse="")
+  actual = paste(flippedCoins, collapse="")
+  match <- grepl(pattern=expected, x=actual, fixed=TRUE)
+  return (match)
+}
+
+
+consecutiveHeads <- function(nTrials, nFlips, kHeads, pHead){
+  count <- 0
+  for(i in 1:nTrials){
+    headsConsecutive <- areThereConsecutiveHeads(kHeads, flipCoin(nFlips, pHead))
+    if(headsConsecutive){
+      count <- count + 1
+    }
+  }
+  cat(fill = TRUE,"probability getting", kHeads, "consecutive heads with pHead(", pHead, ") in ", nFlips,"flips =", count/nTrials)
+}
+
+consecutiveHeads(nTrials = 1000, nFlips = 20, kHeads = 8, pHead = 0.5)
