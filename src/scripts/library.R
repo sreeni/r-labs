c067ae3fd6e60ee1839f3c1d7319e6653a0ec993
Sreenivas Ananthakrishna <sreeni@gmail.com>
2020-06-24 23:22:24 +0530

    fix bug in args to cbind

--- src/scripts/library.R
+++ src/scripts/library.R
@@ -7,18 +7,17 @@ simulateLibraryReturn <- function(nTrials){
     bookReturns <- sample(days, size = nTrials,replace = TRUE, prob = probs )
     foreignLibIndicators  <- sample(c(0,1), size = nTrials, replace = TRUE)
     foreignLibraryDelay = 2
-    bookReturnsWithDelays <- bookReturns + foreignLibraryDelay * foreignLibIndicators
+    b <- bookReturns + foreignLibraryDelay * foreignLibIndicators
 
-    notebook <- cbind(bookReturns, foreignLibIndicators, bookReturnsWithDelays)
+    notebook <- cbind(bookReturns, foreignLibIndicators, b)
     
-    delayedBookReturns <- notebook[bookReturnsWithDelays > currentDay,]
-    #delayedBookReturns <- bookReturnsWithDelays[bookReturnsWithDelays > currentDay] 
+    delayedBookReturns <- notebook[b > currentDay,]
+    result1 <- mean(delayedBookReturns[,3] == waitUntil)
+    cat("result1", result1, fill = TRUE)
     
-    result <- mean(delayedBookReturns[,3] == waitUntil)
-    #
-    #result <- mean(delayedBookReturns == waitUntil)
-    
-    cat("result", result, fill = TRUE)
+    bgrt4 <- b[b > currentDay] 
+    result2 <- mean(bgrt4 == waitUntil)
+    cat("result2", result2, fill = TRUE)
 }
 
-simulateLibraryReturn(nTrials = 1000)
+simulateLibraryReturn(nTrials = 10000)
